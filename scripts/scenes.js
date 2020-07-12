e07724aead33b187fc30f9d6d01cc1482226a781
mellepella <mellepella@gmail.com>
2020-07-12 12:30:28 +0200

    Add one more level
    The total number of playable levels are now: 7.

--- scripts/scenes.js
+++ scripts/scenes.js
@@ -4,7 +4,7 @@ class Scenes {
 			const elapsedSeconds = Game.calculateTime()[0];
 			const elapsedMinutes = Game.calculateTime()[1];
 
-			const winningMessage = `You finished in ${elapsedSeconds} seconds and ${elapsedMinutes} minutes.`;
+			const winningMessage = `You finished in ${elapsedMinutes} minute(s) and ${elapsedSeconds} second(s).`;
 
 			Game.clearCanvas();
 
@@ -65,6 +65,26 @@ const scenes = [
 		CubeCreator.create(ObstacleCube, 11, 7).update();
 		CubeCreator.create(ObstacleCube, 5, 7).update();
 		CubeCreator.create(ObstacleCube, 10, 8).update();
+	},
+	() => {
+		CubeCreator.create(WinningCube, 7, 7).update();
+		CubeCreator.create(ObstacleCube, 9, 7).update();
+		CubeCreator.create(ObstacleCube, 6, 5).update();
+		CubeCreator.create(ObstacleCube, 11, 6).update();
+		CubeCreator.create(ObstacleCube, 10, 1).update();
+		CubeCreator.create(ObstacleCube, 8, 2).update();
+		CubeCreator.create(ObstacleCube, 12, 2).update();
+	},
+	() => {
+		CubeCreator.create(WinningCube, 8, 7).update();
+		CubeCreator.create(ObstacleCube, 6, 4).update();
+		CubeCreator.create(ObstacleCube, 7, 1).update();
+		CubeCreator.create(ObstacleCube, 5, 2).update();
+		CubeCreator.create(ObstacleCube, 11, 3).update();
+		CubeCreator.create(ObstacleCube, 6, 5).update();
+		CubeCreator.create(ObstacleCube, 10, 0).update();
+		CubeCreator.create(ObstacleCube, 10, 6).update();
+		CubeCreator.create(ObstacleCube, 11, 4).update();
 	},
 	() => {
 		Scenes.winningScene();
