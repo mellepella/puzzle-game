511ef98d95261817a52d6977b6845a779d66dcb9
Melvin Wahlberg <mellepella@gmail.com>
2020-08-27 14:26:16 +0200

    Make canvas half as small

--- scripts/scenes.js
+++ scripts/scenes.js
@@ -8,8 +8,8 @@ class Scenes {
 
 			Game.clearCanvas();
 
-			UserInterface.displayText( {x: 4, y: 4, size: "60px",  content: winningMessage} );
-			UserInterface.displayText( {x: 8, y: 5, size: "40px", content: "(Press f5 to restart)"} );	
+			UserInterface.displayText( {x: 4, y: 4, size: "30px",  content: winningMessage} );
+			UserInterface.displayText( {x: 8, y: 5, size: "20px", content: "(Press f5 to restart)"} );	
 			
             gameIsRunning = false;
         }
@@ -20,11 +20,11 @@ class Scenes {
 const scenes = [
   	() => {
 		CubeCreator.create(WinningCube, 6, 4).update();
-		UserInterface.displayText( {x: 5, y: 1, size: '40px', content: 'Get to the yellow Cube to proceed to the next level!'} );
-		UserInterface.displayText( {x: 6.5, y: 2, size: '30px', content: '(Use the W, A, S, D keys to move around)'} );
+		UserInterface.displayText( {x: 5, y: 1, size: '20px', content: 'Get to the yellow Cube to proceed to the next level!'} );
+		UserInterface.displayText( {x: 6.5, y: 2, size: '15px', content: '(Use the W, A, S, D keys to move around)'} );
 	},
 	() => {
-		UserInterface.displayText( {x: 4, y: 1, size: '40px', content: 'Use the obstacles to stop and change the direction you\'re going!'} );
+		UserInterface.displayText( {x: 4, y: 1, size: '20px', content: 'Use the obstacles to stop and change the direction you\'re going!'} );
 		CubeCreator.create(WinningCube, 4, 3).update();
 		CubeCreator.create(ObstacleCube, 7, 2).update();
 		CubeCreator.create(ObstacleCube, 13, 4).update();
@@ -32,7 +32,7 @@ const scenes = [
 		CubeCreator.create(ObstacleCube, 12, 7).update();
 	},
 	() => {
-		UserInterface.displayText( {x: 3.5, y: 1, size: '40px', content: 'If you get stuck, press the R key to restart from the start of the level.'} );
+		UserInterface.displayText( {x: 3.5, y: 1, size: '20px', content: 'If you get stuck, press the R key to restart from the start of the level.'} );
 		CubeCreator.create(WinningCube, 12, 7).update();
 		CubeCreator.create(ObstacleCube, 9, 2).update();
 		CubeCreator.create(ObstacleCube, 6, 4).update();
@@ -42,8 +42,8 @@ const scenes = [
 		CubeCreator.create(ObstacleCube, 11, 7).update();
 	},
 	() => {
-		UserInterface.displayText( {x: 0.5, y: 1, size: '40px', content: 'Try to finish the levels as fast as you can!'} );
-		UserInterface.displayText( {x: 0.5, y: 1.5, size: '25px', content: 'Your time will be displayed in the right upper corner. Good luck!'} );
+		UserInterface.displayText( {x: 0.5, y: 1, size: '20px', content: 'Try to finish the levels as fast as you can!'} );
+		UserInterface.displayText( {x: 0.5, y: 1.5, size: '12.5px', content: 'Your time will be displayed in the right upper corner. Good luck!'} );
 		CubeCreator.create(WinningCube, 9, 1).update();
 		CubeCreator.create(ObstacleCube, 9, 2).update();
 		CubeCreator.create(ObstacleCube, 9, 6).update();
