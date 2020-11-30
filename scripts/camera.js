a2c30a7406d70afc777c00adac6506ed53821448
Melvin Wahlberg <mellepella@gmail.com>
2020-11-30 18:08:41 +0100

    Polish shake() method in Camera

--- scripts/camera.js
+++ scripts/camera.js
@@ -5,28 +5,32 @@ function sleep(ms) {
 class Camera {
   static async shake() {
     const canvas = document.getElementById("canvas");
-    const counter = [1, 2];
+
+    const delay = 7;
+    const counter = [1, 2, 3, 4, 5];
+    const distance = 0.12;
+
     let percentage = 50;
 
     for (const number in counter) {
-      percentage++;
+      percentage += distance;
       canvas.style.marginLeft = `${percentage}%`;
-      await sleep(10);
+      await sleep(delay);
     }
     for (const number in counter) {
-      percentage--;
+      percentage -= distance;
       canvas.style.marginLeft = `${percentage}%`;
-      await sleep(10);
+      await sleep(delay);
     }
     for (const number in counter) {
-      percentage--;
+      percentage -= distance;
       canvas.style.marginLeft = `${percentage}%`;
-      await sleep(10);
+      await sleep(delay);
     }
     for (const number in counter) {
-      percentage++;
+      percentage += distance;
       canvas.style.marginLeft = `${percentage}%`;
-      await sleep(10);
+      await sleep(delay);
     }
   }
 }
