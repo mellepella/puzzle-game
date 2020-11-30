70c32068202767f83efd60abb405706db1fd3aa3
Melvin Wahlberg <mellepella@gmail.com>
2020-11-30 18:11:54 +0100

    Add customizable aggression on screenshake

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,14 +1,10 @@
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 class Camera {
-  static async shake() {
+  static async shake(aggresion) {
     const canvas = document.getElementById("canvas");
 
     const delay = 7;
     const counter = [1, 2, 3, 4, 5];
-    const distance = 0.12;
+    const distance = 0.12 * aggresion;
 
     let percentage = 50;
 
--- scripts/playerCube.js
+++ scripts/playerCube.js
@@ -30,7 +30,7 @@ class PlayerCube extends Cube {
       this.y >= CANVAS_HEIGHT - this.size ||
       this.y <= 0
     ) {
-      Camera.shake();
+      Camera.shake(1);
       Game.restart();
       this.isColliding = false;
     }
