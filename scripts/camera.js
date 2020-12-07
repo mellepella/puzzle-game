b8cdf5335df9b95eaf0ffafe610154dfabf06cce
Melvin Wahlberg <mellepella@gmail.com>
2020-12-07 10:47:07 +0100

    Make shake smooth

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,9 +1,8 @@
 class Camera {
-  static async shake(sleepDelay) {
+  static async shake(sleepDuration) {
     const canvas = document.getElementById("canvas");
 
     const aggression = 1.5;
-    const sleepDuration = sleepDelay;
     const distance = 0.12 * aggression;
     const numberOfFrames = 4;
 
@@ -19,13 +18,13 @@ class Camera {
       canvas.style.marginLeft = `${percentage}%`;
       await sleep(sleepDuration);
     }
-    for (let i = 0; i < numberOfFrames; i++) {
-      percentage -= distance;
+    for (let i = 0; i < numberOfFrames * 2; i++) {
+      percentage -= distance/2;
       canvas.style.marginLeft = `${percentage}%`;
       await sleep(sleepDuration);
     }
-    for (let i = 0; i < numberOfFrames; i++) {
-      percentage += distance;
+    for (let i = 0; i < numberOfFrames * 2; i++) {
+      percentage += distance/2;
       canvas.style.marginLeft = `${percentage}%`;
       await sleep(sleepDuration);
     }
