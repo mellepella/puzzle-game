128459d460fa5efe502a2fcca696c03c8cf1a076
Melvin Wahlberg <mellepella@gmail.com>
2020-12-13 19:47:16 +0100

    Add sandbox mode
    You can now (if debug boolean is true) create levels by clicking at a
    specific position.

--- scripts/addEventListener.js
+++ scripts/addEventListener.js
@@ -1,3 +1,17 @@
-document.onkeypress = function(event)  {
-	Game.detectKeyPress(event);
+document.onkeypress = function (event) {
+  Game.detectKeyPress(event);
+};
+
+function debugListener() {
+  canvas.addEventListener("mousemove", function (event) {
+    UserInterface.getCursorPosition(event);
+  });
+
+  canvas.addEventListener("click", function (event) {
+    Sandbox.addObject(
+      sandboxObject,
+      Math.floor(mouseX / UNIT_SIZE),
+      Math.floor(mouseY / UNIT_SIZE)
+    );
+  });
 }
