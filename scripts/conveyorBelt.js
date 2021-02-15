04ab883f216bd46e6d6ac3225b7171346b6162c4
Melvin Wahlberg <mellepella@gmail.com>
2021-02-15 14:36:05 +0100

    Fix collision

--- scripts/conveyorBelt.js
+++ scripts/conveyorBelt.js
@@ -4,12 +4,15 @@ class ConveyorBelt extends Cube {
     this.direction = direction;
   }
   onCollision(collisionSide) {
-    if (collisionSide === "bottom") {
-      PLAYER_CUBE.stop(PLAYER_CUBE.x, PLAYER_CUBE.y - PLAYER_CUBE.velocityY);
-    } else if (collisionSide === "top") {
-      PLAYER_CUBE.stop(PLAYER_CUBE.x, PLAYER_CUBE.y - PLAYER_CUBE.velocityY);
-    } else if (collisionSide === "left" || collisionSide === "right") {
-      PLAYER_CUBE.stop(PLAYER_CUBE.x - PLAYER_CUBE.velocityX, PLAYER_CUBE.y);
+    switch (collisionSide) {
+      case "top":
+      case "bottom":
+        PLAYER_CUBE.stop(PLAYER_CUBE.x, PLAYER_CUBE.y - PLAYER_CUBE.velocityY);
+        break;
+      case "left":
+      case "right":
+        PLAYER_CUBE.stop(PLAYER_CUBE.x - PLAYER_CUBE.velocityX, PLAYER_CUBE.y);
+        break;
     }
     PLAYER_CUBE.go(KEYS[this.direction.toLowerCase()]);
   }
--- scripts/obstacleCube.js
+++ scripts/obstacleCube.js
@@ -4,12 +4,15 @@ class ObstacleCube extends Cube {
   }
 
   onCollision(collisionSide) {
-    if (collisionSide === "bottom") {
-      PLAYER_CUBE.stop(PLAYER_CUBE.x, PLAYER_CUBE.y - PLAYER_CUBE.velocityY);
-    } else if (collisionSide === "top") {
-      PLAYER_CUBE.stop(PLAYER_CUBE.x, PLAYER_CUBE.y - PLAYER_CUBE.velocityY);
-    } else if (collisionSide === "left" || collisionSide === "right") {
-      PLAYER_CUBE.stop(PLAYER_CUBE.x - PLAYER_CUBE.velocityX, PLAYER_CUBE.y);
+    switch (collisionSide) {
+      case "top":
+      case "bottom":
+        PLAYER_CUBE.stop(PLAYER_CUBE.x, PLAYER_CUBE.y - PLAYER_CUBE.velocityY);
+        break;
+      case "left":
+      case "right":
+        PLAYER_CUBE.stop(PLAYER_CUBE.x - PLAYER_CUBE.velocityX, PLAYER_CUBE.y);
+        break;
     }
   }
 }
